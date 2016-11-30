56e8e1385f032fc90b27f7f6dc413f8113187677
kevin72c <512523616@qq.com>
2016-12-01 00:12:37 +0800

    reconstruct

---
+++ sample/src/main/java/com/cmy/sample/SampleApplication.java
@@ -0,0 +1,14 @@
+package com.cmy.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by kevin on 2016/11/30.
+ */
+@SpringBootApplication
+public class SampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+}
---
+++ sample/src/main/java/com/cmy/sample/controller/DemoController.java
@@ -0,0 +1,7 @@
+package com.cmy.sample.controller;
+
+/**
+ * Created by kevin on 2016/12/1.
+ */
+public class DemoController {
+}
