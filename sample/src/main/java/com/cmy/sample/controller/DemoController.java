6a9eb774dfab00e7dfd64e8cb54e65c7f62b776a
kevin72c <512523616@qq.com>
2016-12-01 09:34:38 +0800

    sample

--- sample/src/main/java/com/cmy/sample/controller/DemoController.java
+++ sample/src/main/java/com/cmy/sample/controller/DemoController.java
@@ -1,7 +1,19 @@
 package com.cmy.sample.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by kevin on 2016/12/1.
  */
+@RestController
+@RequestMapping("demo")
 public class DemoController {
+
+    @GetMapping
+    public String test() {
+        return "success";
+    }
+
 }
