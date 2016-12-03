5cee32662c90d54510bd292f303efef9213bed6c
kevin72c <512523616@qq.com>
2016-12-03 22:43:17 +0800

    demo

--- sample/src/main/java/com/cmy/sample/controller/DemoController.java
+++ sample/src/main/java/com/cmy/sample/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.cmy.sample.controller;
 
+import com.cmy.xcheck.support.annotation.Check;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.Random;
 @RequestMapping("demo")
 public class DemoController {
 
+    @Check({"a@d", "b@w", "c@ml(2)"})
     @GetMapping
     public String test() {
         return "success" + new Random().nextInt(100);
