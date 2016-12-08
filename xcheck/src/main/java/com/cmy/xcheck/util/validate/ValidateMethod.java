e77729ca58665766885962ea7a8f83f18d8b5ae3
kevin72c <512523616@qq.com>
2016-12-08 18:22:15 +0800

    add check object

--- xcheck/src/main/java/com/cmy/xcheck/util/validate/ValidateMethod.java
+++ xcheck/src/main/java/com/cmy/xcheck/util/validate/ValidateMethod.java
@@ -1,10 +1,16 @@
 package com.cmy.xcheck.util.validate;
 
+import com.cmy.xcheck.support.XResult;
+import org.springframework.stereotype.Component;
+
 /**
  * 杭州动享互联网技术有限公司
  * 校验方法接口
  * @Author chenjw
  * @Date 2016年12月08日
  */
+@Component
 public interface ValidateMethod {
+
+    XResult validate(String... args);
 }
--- xcheck/src/main/java/com/cmy/xcheck/util/validate/impl/AllLetter.java
+++ xcheck/src/main/java/com/cmy/xcheck/util/validate/impl/AllLetter.java
@@ -1,11 +1,19 @@
 package com.cmy.xcheck.util.validate.impl;
 
+import com.cmy.xcheck.support.XResult;
 import com.cmy.xcheck.util.validate.ValidateMethod;
+import org.springframework.stereotype.Component;
 
 /**
  * 全字母
  * @Author chenjw
  * @Date 2016年12月08日
  */
+@Component
 public class AllLetter implements ValidateMethod {
+
+    @Override
+    public XResult validate(String... args) {
+        return null;
+    }
 }
